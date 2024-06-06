6d3a4bfd5a0e150953841ffdd4ede009e6ea87e0
narsimharaonijalapur <41208639+narsimharaonijalapur@users.noreply.github.com>
2024-06-06 13:56:51 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -19,7 +19,21 @@ def function():
     # load some data from a blob storage
     logging.info('connect to storage %s', connection_to_storage)
     blob_service_client = BlobServiceClient.from_connection_string(connection_to_storage)
-    blob_client = blob_service_client.get_blob_client(container='<my-container>', blob='<my-blob>')
+    container_name = "sample"
+    blob_container_client = blob_service_client.get_container_client(container_name)
+    blob_container_client_upload = blob_service_client.get_container_client(container_name+"-json")
+    blob_list_uploaded = blob_container_client_upload.list_blobs()
+    uploaded_jsons = [blob.name for blob in blob_list_uploaded]
+    blob_list = blob_container_client.list_blobs()
+    for blob in blob_list:
+        if blob.name not in uploaded_jsons:
+            name = container_name+"/"+blob.name
+            with open(name, "wb") as download_file:
+                download_file.write(blob_client.download_blob().readall())
+            with open(name, "rb") as data:
+                blob_client_upload = blob_container_client_upload.get_blob_client(blob.name)
+                blob_client_upload.upload_blob(data)
+                print("file_uploaded_successfully")
 
 
 def main(mytimer: func.TimerRequest) -> None:
